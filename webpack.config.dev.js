b5449aaa681abdbc502074e8178f871fa43a1a32
Josh Rhoades <josh@axiomdatascience.com>
2022-08-05 16:48:01 -0800

    fix dev server invocation

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    static: {
+        directory: path.join(__dirname, 'dist' )
+    },
   },
 });
